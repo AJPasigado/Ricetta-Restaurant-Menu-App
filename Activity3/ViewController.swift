7c612c0bf3598c201474d811801c05d6f9542c92
AJ Pasigado <ajpasigado@outlook.com>
2018-09-03 19:51:24 +0800

    Initial Commit

---
+++ Activity3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activity3
+//
+//  Created by Anton John Bibit Pasigado on 03/09/2018.
+//  Copyright © 2018 Anton John Pasigado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
